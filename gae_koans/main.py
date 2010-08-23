f8dd68945bfea844263f4057d9938ef1a0fc7524
Barbie <barbarajoyjones@gmail.com>
2010-08-22 21:23:22 -0400

    move along, nothing to see here

--- gae_koans/main.py
+++ gae_koans/main.py
@@ -1,13 +1,12 @@
 """A placeholder."""
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import util
 
 class MainPage(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('this is a placeholder')
 
-application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+APP = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
-if __name__ == '__main__':
-    run_wsgi_app(application)
+util.run_wsgi_app(APP)
