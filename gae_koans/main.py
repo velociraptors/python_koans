a6158349aa33fe831e6071759116d636f3700a5d
Barbie <barbarajoyjones@gmail.com>
2010-08-22 21:45:45 -0400

    properly show the web page

--- gae_koans/main.py
+++ gae_koans/main.py
@@ -1,11 +1,13 @@
-"""A placeholder."""
+"""Stuff to make the app go."""
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import util, template
 
 class MainPage(webapp.RequestHandler):
+    """Make the welcome page happen."""
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('this is a placeholder')
+        path = './templates/index.html'
+        template_values = {}
+        self.response.out.write(template.render(path, template_values))
 
 APP = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
